cb219bf019a0729f8f544854a6ff5ad2f9784e3d
CaptainBoo <68038346+CaptainBoo@users.noreply.github.com>
2022-05-11 09:35:13 -0400

    starting canvas

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-var canvas = document.getElementById("myCanvas");
-
+//Setting up the canvas
+var canvas = document.getElementById("myCanvas");\
 var ctx = canvas.getContext("2d");
+var centerX = canvas.width / 2;
+var centerY = canvas.height / 2;
+
 ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.arc(centerX, centerY, 20, 0, Math.PI*2, false);
 ctx.fillStyle = "green";
 ctx.fill();
 ctx.closePath();
