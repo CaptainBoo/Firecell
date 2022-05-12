c8fd22938e975e2a1464b54874698a97608fb4bb
CaptainBoo <68038346+CaptainBoo@users.noreply.github.com>
2022-05-11 21:01:12 -0400

    get rid of circle

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 //Setting up the canvas
-var canvas = document.getElementById("myCanvas");\
+var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var centerX = canvas.width / 2;
-var centerY = canvas.height / 2;
-
-ctx.beginPath();
-ctx.arc(centerX, centerY, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
