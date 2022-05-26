230dc2c81947957ca25800d44345c908ebed50c9
CaptainBoo <68038346+CaptainBoo@users.noreply.github.com>
2022-05-26 17:55:38 -0400

    header

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 //Setting up the canvas
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
-var centerX = canvas.width / 2;
-var centerY = canvas.height / 2;
-var radius = 70;
