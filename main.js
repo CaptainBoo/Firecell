67cb037eab85a443573670403952837a788c1683
CaptainBoo <eddiethebest06@gmail.com>
2022-12-02 14:43:39 -0500

    following guide 2

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
+const ballRadius = 10;
+
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -19,7 +21,14 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+      if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
     x += dx;
     y += dy;
 }
-  setInterval(draw, 10);
+  
+setInterval(draw, 10);
