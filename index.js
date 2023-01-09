96e0795615e62fa5b2925efe8beb2f65fc899337
CaptainBoo <eddiethebest06@gmail.com>
2023-01-08 21:35:13 -0500

    tried express amd changed font

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ function returnText(){
     playerName = document.getElementById('inputPlayerName').value;
     alert(playerName)
 }
-
-import * as testname from './test.json';
-const {name} = testname;
-console.log(name); // output 'testing'
