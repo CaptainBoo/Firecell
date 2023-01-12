cd5ff86dc593eb208c10089ad381ab7221c37841
CaptainBoo <eddiethebest06@gmail.com>
2023-01-12 12:21:34 -0500

    moved index html to root

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const app = express();
 const port = 3000;
 app.use(express.static('public'));
 
+app.get('/',function(req,res){
+    res.sendfile(__dirname + '/index.html');
+});
+
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
