1f7859ae4ceabc6964d75be2900e3462039238e3
CaptainBoo <eddiethebest06@gmail.com>
2023-01-15 22:07:02 -0500

    implements mojang api #1

--- public/app.js
+++ public/app.js
@@ -1,5 +1,11 @@
+import fetch from 'node-fetch';
+
 var playerName;
 function returnText() {
 	playerName = document.getElementById('inputPlayerName').value;
 	document.getElementById('playerName').innerText = playerName;
 }
+
+fetch('https://api.mojang.com/users/profiles/minecraft/Captain_Boo')
+  .then(res => res.json())
+  .then(console.log)
