0e4eab98f62454aa845ce5704453341c038309c7
CaptainBoo <eddiethebest06@gmail.com>
2023-01-17 12:17:38 -0500

    no cors

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const app = express();
 const port = 3000;
 app.use(express.static('public'));
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, () => {
--- public/getapi.js
+++ public/getapi.js
@@ -1,7 +1,8 @@
-import fetch from 'node-fetch';
+var UUIDButton = document.getElementById('submitUUID');
+UUIDButton.addEventListener('click', returnUUID);
 function returnUUID() {
 	playerName = document.getElementById('inputPlayerName').value;
-	fetch('https://api.mojang.com/users/profiles/minecraft/${playerName}')
+	fetch('https://api.mojang.com/users/profiles/minecraft/${playerName}', {mode:'no-cors'})
 		.then((res) => res.json())
 		.then(console.log);
 }
