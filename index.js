9894cee1d62316b9aa4cbd3d516473ccda96e3c5
CaptainBoo <eddiethebest06@gmail.com>
2023-01-18 12:20:15 -0500

    fix this later

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ import 'dotenv/config';
 
 import express from 'express';
 const app = express();
+
+
 const port = 3000;
+
+app.use(function( req, res, next ) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+});
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
@@ -18,3 +27,7 @@ app.get('/', function (req, res) {
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
+function newFunction() {
+	return require('cors');
+}
+
--- public/getapi.js
+++ public/getapi.js
@@ -2,7 +2,7 @@ var UUIDButton = document.getElementById('submitUUID');
 UUIDButton.addEventListener('click', returnUUID);
 function returnUUID() {
 	playerName = document.getElementById('inputPlayerName').value;
-	fetch('https://api.mojang.com/users/profiles/minecraft/${playerName}', {mode:'no-cors'})
-		.then((res) => res.json())
-		.then(console.log);
+	fetch('https://api.mojang.com/users/profiles/minecraft/${playerName}')
+		.then((response) => response.json())
+		.then((json) => console.log(json));
 }
