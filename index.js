2cd5bc4109d9ac4dd6a36a20c04d13fdb95a892c
CaptainBoo <eddiethebest06@gmail.com>
2023-01-18 19:47:40 -0500

    start here

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
@@ -27,6 +19,3 @@ app.get('/', function (req, res) {
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
-function newFunction() {
-	return require('cors');
-}
