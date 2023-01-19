9bd1c3e361bee614033508e10909972037d134c9
CaptainBoo <eddiethebest06@gmail.com>
2023-01-18 20:40:06 -0500

    IT WORKS

--- public/app.js
+++ public/app.js
@@ -1,5 +1,12 @@
-var playerName;
-function returnText() {
+var UUIDButton = document.getElementById('submitUUID');
+UUIDButton.addEventListener('click', returnUUID);
+var UUIDResponse
+function returnUUID() {
 	playerName = document.getElementById('inputPlayerName').value;
-	document.getElementById('playerName').innerText = playerName;
+	fetch(`https://api.ashcon.app/mojang/v2/user/${playerName}`)
+		.then((response) => response.json())
+		.then((data) => document.getElementById('returnUUID').innerText = data.uuid)
+		.catch((error) => {
+			console.log(error);
+		});
 }
--- public/getapi.js
+++
@@ -1,8 +0,0 @@
-var UUIDButton = document.getElementById('submitUUID');
-UUIDButton.addEventListener('click', returnUUID);
-function returnUUID() {
-	playerName = document.getElementById('inputPlayerName').value;
-	fetch('https://api.mojang.com/users/profiles/minecraft/${playerName}')
-		.then((response) => response.json())
-		.then((json) => console.log(json));
-}
