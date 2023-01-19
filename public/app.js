1631f488c7991f9f12a7c55c8996022637d56949
CaptainBoo <eddiethebest06@gmail.com>
2023-01-19 09:40:02 -0500

    need to add work with enter

--- public/app.js
+++ public/app.js
@@ -1,11 +1,15 @@
 var UUIDButton = document.getElementById('submitUUID');
-UUIDButton.addEventListener('click', returnUUID);
-var UUIDResponse
+UUIDButton.addEventListener('click', returnUUID)
+
+var UUIDResponse;
 function returnUUID() {
 	playerName = document.getElementById('inputPlayerName').value;
 	fetch(`https://api.ashcon.app/mojang/v2/user/${playerName}`)
 		.then((response) => response.json())
-		.then((data) => document.getElementById('returnUUID').innerText = data.uuid)
+		.then(
+			(data) =>
+				(document.getElementById('returnUUID').innerText = data.uuid)
+		)
 		.catch((error) => {
 			console.log(error);
 		});
