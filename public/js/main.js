9b82ce77797445b769f0ee4cd5b21514ecf97ee8
CaptainBoo <eddiethebest06@gmail.com>
2023-02-07 12:16:35 -0500

    Cannot use import statement outside a module

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,61 @@
+import Dungeon from '@mikewesthad/dungeon';
+
 var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+	type: Phaser.AUTO,
+	width: 800,
+	height: 480,
+	backgroundColor: '#222222',
+    parent: "game-container",
+	pixelArt: true,
+	scene: {
+		preload: preload,
+		create: create,
+		update: update,
+	},
+	physics: {
+		default: 'arcade',
+		arcade: {
+			tileBias: 4,
+		},
+	},
 };
 
 var game = new Phaser.Game(config);
 
-function preload ()
-{
-}
+function preload() {}
 
-function create ()
-{
-}
+function create() {}
 
-function update ()
-{
-}
+function update() {}
+
+const dungeon = new Dungeon({
+	// The dungeon's grid size
+	width: 40,
+	height: 40,
+	rooms: {
+		// Random range for the width of a room (grid units)
+		width: {
+			min: 5,
+			max: 10,
+		},
+		// Random range for the height of a room (grid units)
+		height: {
+			min: 8,
+			max: 20,
+		},
+		// Cap the area of a room - e.g. this will prevent large rooms like 10 x 20
+		maxArea: 150,
+		// Max rooms to place
+		maxRooms: 10,
+	},
+});
+
+const html = dungeon.drawToHtml({
+	empty: ' ',
+	wall: '📦',
+	floor: '☁️',
+	door: '🚪',
+});
+
+// Append the element to an existing element on the page
+document.body.appendChild(html);
