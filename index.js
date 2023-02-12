85492b9c0be99a0c6ccbf2e746be43fa7d565cbc
CaptainBoo <eddiethebest06@gmail.com>
2023-02-12 15:54:51 -0500

    first parcel test

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + '/public/'));
 app.use(express.static('dist'));
-
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + 'index.html');
-});
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
